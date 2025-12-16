99773aadc6126d72aa923fde4a622149899fcce3
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-16 21:02:42 +0530

    crud application

--- INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/controller/JournalEntryController.java
+++ INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/controller/JournalEntryController.java
@@ -1,8 +1,57 @@
 package com.dailycodebuffer.springbootdemo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dailycodebuffer.springbootdemo.entity.JournalEntry;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
+          public Map<Long,JournalEntry> journalEntries = new HashMap<>();
+         
+          @GetMapping
+          public List<JournalEntry> getAllJournalEntries() {
+                    return new ArrayList<>(journalEntries.values());
+          }
+
+          @PostMapping
+          public boolean createEntry(@RequestBody JournalEntry entity) {
+                    journalEntries.put(entity.getId(), entity);
+                    return true;
+          }
+
+          @GetMapping("id/{myId}")
+          public JournalEntry getJournalEntryById(@PathVariable Long myId) {
+                    return journalEntries.get(myId);
+          }
+
+          @DeleteMapping("id/{myId}")
+          public boolean deleteEntryById(@PathVariable Long myId) {
+                    journalEntries.remove(myId);
+                    return true;
+          }
+          @PutMapping("id/{myId}")
+          public JournalEntry updaEntryById(@PathVariable Long myId, @RequestBody JournalEntry entity) {
+                    journalEntries.put(myId, entity);
+                    return journalEntries.get(myId);
+          }
+
+          
 }
--- INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/entity/JournalEntry.java
+++ INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/entity/JournalEntry.java
@@ -6,29 +6,27 @@ public class JournalEntry {
           private String content;
 
           public long getId() {
-    return id;
-}
+                    return id;
+          }
 
-public void setId(long id) {
-    this.id = id;
-}
+          public void setId(long id) {
+                    this.id = id;
+          }
 
-public String getTitle() {
-    return title;
-}
+          public String getTitle() {
+                    return title;
+          }
 
-public void setTitle(String title) {
-    this.title = title;
-}
+          public void setTitle(String title) {
+                    this.title = title;
+          }         
 
-public String getContent() {
-    return content;
-}
+          public String getContent() {
+                    return content;
+          }
 
-public void setContent(String content) {
-    this.content = content;
-}
-
-
+          public void setContent(String content) {
+                    this.content = content;
+          }
 
 }
