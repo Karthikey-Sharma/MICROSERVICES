f0fd6cb6275ccdc42de3c416a0303851b4a40517
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-16 20:58:57 +0530

    made a crud application using spring boot

--- INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/JournalAppApplication.java
+++ INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/JournalAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class JournalAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		SpringApplication.run(JournalAppApplication.class, args);
 	}
 
 }
---
+++ INTRODUCTION_TO_SPRINGBOOT/src/main/java/com/dailycodebuffer/springbootdemo/controller/HealthCheck.java
@@ -0,0 +1,14 @@
+package com.dailycodebuffer.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HealthCheck {
+          @GetMapping("/health-check")
+          public String healthCheck(){
+                    return "Ok";
+          }
+}
