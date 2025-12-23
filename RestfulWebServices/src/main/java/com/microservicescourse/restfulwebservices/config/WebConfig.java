114be7dd30b9abae05820ff875d773e0ada20c60
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-23 22:36:35 +0530

    Context Configuration

--- RestfulWebServices/src/main/java/com/microservicescourse/restfulwebservices/config/WebConfig.java
+++ RestfulWebServices/src/main/java/com/microservicescourse/restfulwebservices/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.microservicescourse.restfulwebservices.config;
 
-public class WebConfig {
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    // this will be through query parameter
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .ignoreAcceptHeader(true)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json" , MediaType.APPLICATION_JSON);
+    }
+
 }
